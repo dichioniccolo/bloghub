cc4bb1afdadd6dbdf81a50a18e5746ef147d2b17
Niccolò Di Chio <dichioniccolo@gmail.com>
2023-05-11 21:40:06 +0200

    fix: mailing not included in transpile packages and so email sent was empty

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -1,17 +1,17 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds and Linting.
- */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+// Importing env files here to validate on build
+import "@acme/auth/env.mjs";
+import "@acme/db/env.mjs";
+import "@acme/mailing/env.mjs";
+import "./src/env.mjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/db"],
+  transpilePackages: ["@acme/auth", "@acme/db", "@acme/mailing", "@acme/ui"],
   /** We already do linting and typechecking as separate tasks in CI */
-  eslint: { ignoreDuringBuilds: !!process.env.CI },
-  typescript: { ignoreBuildErrors: !!process.env.CI },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   experimental: {
     serverActions: true,
   },
--- apps/dashboard/src/env.mjs
+++ apps/dashboard/src/env.mjs
@@ -7,18 +7,8 @@ export const env = createEnv({
    * built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     EDGE_CONFIG: z.string().url(),
-    NEXTAUTH_SECRET: z.string().min(1),
     NODE_ENV: z.enum(["development", "test", "production"]),
-    POSTMARK_API_KEY: z.string().min(1),
-    POSTMARK_BROADCAST_ACCESS_KEY: z.string().min(1),
-    POSTMARK_BROADCAST_SECRET_KEY: z.string().min(1),
-    POSTMARK_FROM: z.preprocess(
-      // get from the string the value between the < and > characters
-      (str) => (typeof str === "string" ? str.match(/<(.*)>/)?.[1] : null),
-      z.string().email(),
-    ),
     VERCEL_API_URL: z.string().min(1),
     VERCEL_BEARER_TOKEN: z.string().min(1),
     VERCEL_ENABLE_DOMAIN: z
@@ -39,15 +29,9 @@ export const env = createEnv({
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     EDGE_CONFIG: process.env.EDGE_CONFIG,
     NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NODE_ENV: process.env.NODE_ENV,
-    POSTMARK_API_KEY: process.env.POSTMARK_API_KEY,
-    POSTMARK_BROADCAST_ACCESS_KEY: process.env.POSTMARK_BROADCAST_ACCESS_KEY,
-    POSTMARK_BROADCAST_SECRET_KEY: process.env.POSTMARK_BROADCAST_SECRET_KEY,
-    POSTMARK_FROM: process.env.POSTMARK_FROM,
     VERCEL_API_URL: process.env.VERCEL_API_URL,
     VERCEL_BEARER_TOKEN: process.env.VERCEL_BEARER_TOKEN,
     VERCEL_ENABLE_DOMAIN: process.env.VERCEL_ENABLE_DOMAIN,
