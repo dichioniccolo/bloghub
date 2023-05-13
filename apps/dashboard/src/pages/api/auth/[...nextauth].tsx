e4307246ba958d9526e6bc538f84d2a665e4c63c
Niccolò Di Chio <dichioniccolo@gmail.com>
2023-05-13 18:58:35 +0200

    feat: use postmark client to send emails

--- apps/dashboard/src/pages/api/auth/[...nextauth].tsx
+++ apps/dashboard/src/pages/api/auth/[...nextauth].tsx
@@ -1,35 +1,56 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
+import { Client } from "postmark";
 
 import { authOptions } from "@acme/auth";
-import {
-  LoginLink,
-  WelcomeEmail,
-  sendMail,
-  sendMarketingMail,
-} from "@acme/emails";
+import { WelcomeEmail, sendMarketingMail } from "@acme/emails";
 
 import { env } from "~/env.mjs";
+
+// Send an email:
+const client = new Client(env.POSTMARK_API_KEY);
 
 export default NextAuth({
   ...authOptions,
   providers: [
     EmailProvider({
-      async sendVerificationRequest({ identifier, url }) {
+      from: env.POSTMARK_FROM,
+      async sendVerificationRequest({ identifier, url, provider }) {
         if (identifier !== "dichioniccolo@gmail.com") {
           return;
         }
 
-        await sendMail({
-          to: identifier,
-          subject: "Your login link",
-          component: <LoginLink url={url} />,
-          headers: {
-            // Set this to prevent Gmail from threading emails.
-            // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
-            "X-Entity-Ref-ID": `${new Date().getTime()}`,
+        const result = await client.sendEmailWithTemplate({
+          TemplateId: env.POSTMARK_LOGIN_LINK_TEMPLATE_ID,
+          To: identifier,
+          From: provider.from ?? env.POSTMARK_FROM,
+          TemplateModel: {
+            url,
           },
+          Headers: [
+            {
+              // Set this to prevent Gmail from threading emails.
+              // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
+              Name: "X-Entity-Ref-ID",
+              Value: `${new Date().getTime()}`,
+            },
+          ],
         });
+
+        if (result.ErrorCode) {
+          throw new Error(result.Message);
+        }
+
+        // await sendMail({
+        //   to: identifier,
+        //   subject: "Your login link",
+        //   component: <LoginLink url={url} />,
+        //   headers: {
+        //     // Set this to prevent Gmail from threading emails.
+        //     // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
+        //     "X-Entity-Ref-ID": `${new Date().getTime()}`,
+        //   },
+        // });
       },
     }),
     /**
