628eac98e46cff19f5dab450a496bdb8edbc6130
Niccolò Di Chio <dichioniccolo@gmail.com>
2023-06-01 17:51:13 +0200

    fix: settings layout route params

--- apps/dashboard/src/app/(dashboard)/projects/[projectId]/(main)/settings/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/projects/[projectId]/(main)/settings/layout.tsx
@@ -4,18 +4,18 @@ import { DashboardHeader } from "~/app/_components/dashboard-header";
 import { DashboardShell } from "~/app/_components/dashboard-shell";
 import { ProjectSettingsNavigationMenu } from "./_components/project-settings-navigation-menu";
 
-type Props = { params: { id: string } };
+type Props = { params: { projectId: string } };
 
 export default function Layout({
   children,
-  params: { id },
+  params: { projectId },
 }: PropsWithChildren<Props>) {
   return (
     <DashboardShell>
       <DashboardHeader heading="Project Settings" />
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
         <aside className="-mx-4 lg:w-1/5">
-          <ProjectSettingsNavigationMenu projectId={id} />
+          <ProjectSettingsNavigationMenu projectId={projectId} />
         </aside>
         <div className="flex-1">{children}</div>
       </div>
