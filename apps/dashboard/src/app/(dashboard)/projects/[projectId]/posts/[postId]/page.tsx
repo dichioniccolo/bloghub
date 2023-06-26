1d73b26d8c2dad52c441c1c352eafa5fa0710503
Niccolo Di Chio <dichioniccolo@gmail.com>
2023-06-26 20:34:08 +0200

    feat: start publish sheet

--- apps/dashboard/src/app/(dashboard)/projects/[projectId]/posts/[postId]/page.tsx
+++ apps/dashboard/src/app/(dashboard)/projects/[projectId]/posts/[postId]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { getPost } from "~/app/_api/posts";
 import { EditPostForm } from "./_components/edit-post-form";
+import { PublishSheet } from "./_components/publish-sheet";
 
 type Props = {
   params: {
@@ -19,6 +20,9 @@ export default async function Page({ params: { projectId, postId } }: Props) {
 
   return (
     <div className="mt-4 space-y-4">
+      <div className="flex items-center justify-end">
+        <PublishSheet post={post} />
+      </div>
       <EditPostForm post={post} />
     </div>
   );
