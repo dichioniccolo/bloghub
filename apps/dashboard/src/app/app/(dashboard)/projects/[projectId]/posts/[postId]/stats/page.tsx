c0b2f5684eec0e439020d04453e366f06797ed86
Niccolo Di Chio <dichioniccolo@gmail.com>
2023-07-14 16:56:03 +0200

    feat: finish post stats page

--- apps/dashboard/src/app/app/(dashboard)/projects/[projectId]/posts/[postId]/stats/page.tsx
+++ apps/dashboard/src/app/app/(dashboard)/projects/[projectId]/posts/[postId]/stats/page.tsx
@@ -1,3 +1,72 @@
-export default function Stats() {
-  return <div>stats</div>;
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+import { getPost, getPostAnalytics } from "~/app/_api/posts";
+import { getProjectOwner } from "~/app/_api/projects";
+import { Analytics } from "../../../(main)/stats/_components/analytics";
+
+type Props = {
+  params: {
+    projectId: string;
+    postId: string;
+  };
+};
+
+export default async function Stats({ params: { projectId, postId } }: Props) {
+  const post = await getPost(projectId, postId);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const owner = await getProjectOwner(projectId);
+
+  if (owner.usage > owner.quota) {
+    return (
+      <>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-4">
+            <h1 className="text-3xl font-bold">
+              Analytics for {post.project.name} - {post.title ?? post.slug}
+            </h1>
+            <Link
+              href={`https://${post.project.domain}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="bg-stone truncate rounded-md px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200"
+            >
+              {post.project.domain} ↗
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          You have exceeded your monthly clicks. You won&apos; have access to
+          statistics until you upgrade or wait for the next billing month
+        </div>
+      </>
+    );
+  }
+
+  const analytics = await getPostAnalytics(projectId, postId);
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-4">
+          <h1 className="text-3xl font-bold">
+            Analytics for {post.project.name} - {post.title ?? post.slug}
+          </h1>
+          <Link
+            href={`https://${post.project.domain}`}
+            target="_blank"
+            rel="noreferrer noopener"
+            className="bg-stone truncate rounded-md px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200"
+          >
+            {post.project.domain} ↗
+          </Link>
+        </div>
+      </div>
+      <Analytics analytics={analytics} />
+    </>
+  );
 }
