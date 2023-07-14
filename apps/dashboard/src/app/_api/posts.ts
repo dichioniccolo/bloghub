dbb28bdb7c71d08a7ded35d4a66c5c00edcd0241
Niccolo Di Chio <dichioniccolo@gmail.com>
2023-07-14 16:56:03 +0200

    feat: finish post stats page

--- apps/dashboard/src/app/_api/posts.ts
+++ apps/dashboard/src/app/_api/posts.ts
@@ -1,6 +1,16 @@
 "use server";
 
-import { and, db, eq, posts, projectMembers, sql, visits } from "@bloghub/db";
+import {
+  and,
+  db,
+  desc,
+  eq,
+  posts,
+  projectMembers,
+  projects,
+  sql,
+  visits,
+} from "@bloghub/db";
 
 import { $getUser } from "./get-user";
 
@@ -43,8 +53,13 @@ export async function getPost(projectId: string, postId: string) {
       createdAt: posts.createdAt,
       hidden: posts.hidden,
       content: posts.content,
+      project: {
+        name: projects.name,
+        domain: projects.domain,
+      },
     })
     .from(posts)
+    .innerJoin(projects, eq(projects.id, posts.projectId))
     .innerJoin(
       projectMembers,
       and(
@@ -57,3 +72,91 @@ export async function getPost(projectId: string, postId: string) {
 }
 
 export type GetPost = Awaited<ReturnType<typeof getPost>>;
+
+export async function getPostAnalytics(projectId: string, postId: string) {
+  const user = await $getUser();
+
+  const clicksMyMonth = await db
+    .select({
+      year: sql<number>`YEAR(${visits.createdAt})`.mapWith(Number),
+      month: sql<number>`MONTH(${visits.createdAt})`.mapWith(Number),
+      count: sql<number>`count(*)`.mapWith(Number),
+    })
+    .from(visits)
+    .innerJoin(
+      projectMembers,
+      and(
+        eq(projectMembers.projectId, visits.projectId),
+        eq(projectMembers.userId, user.id),
+      ),
+    )
+    .where(and(eq(visits.projectId, projectId), eq(visits.postId, postId)))
+    .orderBy(sql`YEAR(${visits.createdAt})`, sql`MONTH(${visits.createdAt})`)
+    .groupBy(sql`YEAR(${visits.createdAt})`, sql`MONTH(${visits.createdAt})`);
+
+  const topPosts = await db
+    .select({
+      id: visits.postId,
+      slug: sql`coalesce(${posts.slug}, 'DELETED')`,
+      count: sql<number>`count(*)`.mapWith(Number),
+    })
+    .from(visits)
+    .leftJoin(posts, eq(posts.id, visits.postId))
+    .innerJoin(
+      projectMembers,
+      and(
+        eq(projectMembers.projectId, visits.projectId),
+        eq(projectMembers.userId, user.id),
+      ),
+    )
+    .where(and(eq(visits.projectId, projectId), eq(visits.postId, postId)))
+    .limit(5)
+    .groupBy(visits.postId)
+    .orderBy(desc(sql`count(*)`));
+
+  const topCountries = await db
+    .select({
+      country: sql<string>`coalesce(${visits.geoCountry}, 'Other')`,
+      count: sql<number>`count(*)`.mapWith(Number),
+    })
+    .from(visits)
+    .innerJoin(
+      projectMembers,
+      and(
+        eq(projectMembers.projectId, visits.projectId),
+        eq(projectMembers.userId, user.id),
+      ),
+    )
+    .where(and(eq(visits.projectId, projectId), eq(visits.postId, postId)))
+    .limit(5)
+    .groupBy(visits.geoCountry)
+    .orderBy(desc(sql`count(*)`));
+
+  const topCities = await db
+    .select({
+      country: sql<string>`coalesce(${visits.geoCountry}, 'Other')`,
+      city: sql<string>`coalesce(${visits.geoCity}, 'Other')`,
+      count: sql<number>`count(*)`.mapWith(Number),
+    })
+    .from(visits)
+    .innerJoin(
+      projectMembers,
+      and(
+        eq(projectMembers.projectId, visits.projectId),
+        eq(projectMembers.userId, user.id),
+      ),
+    )
+    .where(and(eq(visits.projectId, projectId), eq(visits.postId, postId)))
+    .limit(5)
+    .groupBy(visits.geoCountry, visits.geoCity)
+    .orderBy(desc(sql`count(*)`));
+
+  return {
+    clicksMyMonth,
+    topPosts,
+    topCountries,
+    topCities,
+  };
+}
+
+export type GetPostAnalytics = Awaited<ReturnType<typeof getPostAnalytics>>;
