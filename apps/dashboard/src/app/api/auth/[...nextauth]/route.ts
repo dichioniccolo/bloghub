d333a16f1e98e1225e6791bddb8941ba46936718
Niccolò Di Chio <dichioniccolo@gmail.com>
2023-10-15 22:00:43 +0200

    feat: also auth routes should be on edge

--- apps/dashboard/src/app/api/auth/[...nextauth]/route.ts
+++ apps/dashboard/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
+import type { ServerRuntime } from "next";
+
 import { authHandlers } from "~/lib/auth";
 
-// export const runtime: ServerRuntime = "edge";
+export const runtime: ServerRuntime = "edge";
 
 export const { GET, POST } = authHandlers;
