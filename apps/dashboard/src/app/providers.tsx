d57b3d4f586e326c691f478cf61b82778d9ef794
Niccolò Di Chio <dichioniccolo@gmail.com>
2023-12-23 21:24:25 +0100

    feat: use new sonner through shadcn

--- apps/dashboard/src/app/providers.tsx
+++ apps/dashboard/src/app/providers.tsx
@@ -1,23 +1,15 @@
 "use client";
 
 import type { PropsWithChildren } from "react";
-import { ThemeProvider, useTheme } from "next-themes";
-import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
 
+import { Toaster } from "@acme/ui/components/ui/sonner";
 import { TooltipProvider } from "@acme/ui/components/ui/tooltip";
-
-const ToasterProvider = () => {
-  const { theme } = useTheme() as {
-    theme: "light" | "dark" | "system";
-  };
-
-  return <Toaster theme={theme} closeButton richColors />;
-};
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <ThemeProvider attribute="class">
-      <ToasterProvider />
+      <Toaster />
       <TooltipProvider>{children}</TooltipProvider>
     </ThemeProvider>
   );
