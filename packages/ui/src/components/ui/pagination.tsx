907453ec7e186b9e2d62f0d7970c330fa5dd1aeb
Niccolò Di Chio <dichioniccolo@gmail.com>
2024-01-29 22:33:36 +0100

    Revert "fix: pagination links"
    This reverts commit 5b304fb52b9ec3bcee9f37e457c265932d448f74.

--- packages/ui/src/components/ui/pagination.tsx
+++ packages/ui/src/components/ui/pagination.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import Link from "next/link";
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
 
 import { cn } from "../../lib/utils";
+import { Link } from "../link";
 import type { ButtonProps } from "./button";
 import { buttonVariants } from "./button";
 
