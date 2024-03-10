42f8454f031f24f72b093c40889478d114f98661
Niccolò Di Chio <dichioniccolo@gmail.com>
2024-03-10 11:02:41 +0100

    fix: prisma client in dev should be instantiated once

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,6 +2,8 @@ import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
+export * from "@prisma/client";
+
 // import ws from "ws";
 
 export { createId } from "@paralleldrive/cuid2";
@@ -16,6 +18,17 @@ const pool = new Pool({
 
 const adapter = new PrismaNeon(pool);
 
-export const prisma = new PrismaClient({ adapter });
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    adapter,
+  });
+};
 
-export * from "@prisma/client";
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
